b47683131c3ef91528c37e33b289c815aa81ae16
Greg Durfee <gregdurfee@gregdurfeeimac.local>
2018-12-05 08:31:57 -0700

    Initial Commit

---
+++ HighTowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HighTowers
+//
+//  Created by Greg Durfee on 12/5/18.
+//  Copyright © 2018 Durfee's Sandbox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
